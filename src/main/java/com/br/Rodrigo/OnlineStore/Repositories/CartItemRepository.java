6b55dfef70fc95b55f4fda3457daa210ad883764
Rodrigo <rocostaparis@hotmail.com>
2023-09-22 11:08:29 -0300

    feat: add OrderItemRepository

--- src/main/java/com/br/Rodrigo/OnlineStore/Repositories/CartItemRepository.java
+++ src/main/java/com/br/Rodrigo/OnlineStore/Repositories/CartItemRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartItemRepository extends JpaRepository<Long, CartItem>{
+public interface CartItemRepository extends JpaRepository<CartItem, Long>{
     
 
 }
--- src/main/java/com/br/Rodrigo/OnlineStore/Repositories/OrderItemRepository.java
+++ src/main/java/com/br/Rodrigo/OnlineStore/Repositories/OrderItemRepository.java
@@ -1,12 +1,12 @@
 package com.br.Rodrigo.OnlineStore.Repositories;
 
-import com.br.Rodrigo.OnlineStore.Domain.User;
+import com.br.Rodrigo.OnlineStore.Domain.OrderItem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
     
 	
 }
